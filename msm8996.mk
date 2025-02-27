fff0ace9648d257f433a7742a4d997ff164fc0f6
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    msm8996-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- msm8996.mk
+++ msm8996.mk
@@ -237,7 +237,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # Kernel
-PRODUCT_ENABLE_UFFD_GC := false
+PRODUCT_ENABLE_UFFD_GC := true
 
 # Keymaster
 PRODUCT_PACKAGES += \
